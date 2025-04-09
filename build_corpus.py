2935f63dae4b3aa930dd8722223e70b5653bd334
jwin2000 <38480717+jwin2000@users.noreply.github.com>
2025-04-09 18:40:34 -0400

    It works!

--- build_corpus.py
+++ build_corpus.py
@@ -1,62 +1,67 @@
 '''
-Python Script to build a corpus for a new Bible
+Python Script to build a corpus for a spicier Old Testament
 Jonah Winchell
 Code as a Liberal Art, Spring 2025
 '''
 
-import json
 import re
 import requests
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup as bs, SoupStrainer
 
-index = 'https://www.sefaria.org/Genesis.1.1?lang=en&aliyot=0'
-
-response = requests.get(index)
-# start = 'DJANGO_VARS'
-# end = 'STRAPI_INSTANCE'
-# m = re.compile(r'DJANGO_VARS(.*?)', re.MULTILINE)
-# n = re.compile(r'(.*?)STRAPI_INSTANCE', re.MULTILINE)
-
-# x = re.compile(r"^(.+?)(DJANGO_VARS).*\n((?:\n.+)+)", re.MULTILINE)
-# x = re.compile(r'(DJANGO_VARS).*(STRAPI_INSTANCE)', re.DOTALL)
-# print(re.search(x, response).string)
-# exit()
+index = 'https://archive.org/download/kjv-text-files'
+index2 = 'https://dn721603.ca.archive.org/0/items/tolkien-j.-the-lord-of-the-rings-harper-collins-ebooks-2010/Tolkien-J.-The-lord-of-the-rings-HarperCollins-ebooks-2010_djvu.txt'
 
 
-start = re.search('DJANGO_VARS', response.text).span()[0]+14
-end = re.search('STRAPI_INSTANCE', response.text).span()[0]-4
-j = response.text[start:end]
-clean = re.sub(r'\n|\r|\t', '', j)
-#clean = re.sub(r'(?<!\")\:', '"props"', clean)
-print(clean)
-exit()
-print(clean)
-new = json.loads(clean)
-print(new)
-exit()
+''' Scrape and download the Torah '''
+def download_torah():
+    torah_books = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy']
+
+    p = re.compile(r'^[A-Z].*')
+    response = requests.get(index)
+    soup = bs(response.content, 'html.parser', parse_only=SoupStrainer('tr'))
+    soup.prettify()
+    for link in soup.find_all('a'):
+        if link.has_attr('href') and re.match(p, link['href']):
+            link_name = link['href'].split('.')[0]
+            if link_name in torah_books:
+                new_response = requests.get(f'{index}/{link_name}.txt')
+                text = str(bs(new_response.content, 'html.parser'))
+
+                with open(f'corpora/{link_name}.txt', 'w') as f:
+                    f.write(text)
 
 
-with open('corpora/c1.txt', 'r') as f:
-    txt = f.read()
-    
-    
-    #print(re.search(m, response))
-    print(re.search('DJANGO_VARS', txt).span()[0])
-    print(re.search('STRAPI_INSTANCE', txt).span()[0]-1)
+''' Scrape and download the Lord of the Rings '''
+def download_LOTR():
+    # Warning, absolutely terrible methods to clean the text lie below
+    p = re.compile(r'^[a-zA-Z].*')
+    p2 = re.compile(r'.*[1-9].*')
+    include = False
+
+    # Get text from website
+    response = requests.get(index2)
+    text = str(bs(response.content, 'html.parser'))
+    lines = list(iter(text.splitlines()))
+    final_text = ''
+
+    # Go through each line of text
+    for l in lines:
+        if include:
+            # Stop when we hit the appendix
+            if 'APPENDIX' in l:
+                break
+            # If the line contains valid text, include it
+            elif re.match(p, l) and not re.match(p2, l):
+                final_text += f'{l}\n'
+        # If we have not yet hit the prologue, do not include
+        elif 'accessory volume.' in l:
+            include = True
+
+    # Save text to local file
+    with open(f'corpora/lotr.txt', 'w') as f:
+        f.write(final_text)
 
 
-soup = bs(response.content, features='html.parser')
-soup.prettify()
-print(response)
-#exit()
-# results = soup.find_all('tr', attrs={'class': 'Co_Verse'})
-results = soup.find_all('tr')
-print(results)
-
-exit()
-
-with open('corpora/c1.txt', 'w') as f:
-    for r in results:
-        f.write(str(r))
-
-#print(soup)
+if __name__ == '__main__':
+    download_torah()
+    download_LOTR()
--- markov.py
+++ markov.py
@@ -1,33 +1,11 @@
 '''
-Python Script to build a corpus for a new Bible
+Python Script to spice up the Old Testament
 Jonah Winchell
 Code as a Liberal Art, Spring 2025
-'''
 
-'''
-Original object stores key word, following words with number of occurences,
-and total number of occurences of this word
 
-temp = {
-    'I': [
-        {
-            'am': 5,
-            'will': 7,
-            'do': 19
-        },
-        31
-    ],
-    'will': [
-        {
-            'not': 7,
-            'never': 9
-        },
-        16
-    ]
-}
-
-Final object stores key word and list of following words with their 
-chance of occurence
+Markov Chain object stores key words and a list of following words 
+with their chance of occurence
 
 markov_chain = {
     'I': {
@@ -43,35 +21,54 @@ markov_chain = {
 '''
 
 import re
+import os
+import json
 import random
 
 
 def main():
-    cleaned_text = []
-    # Open and clean the raw text
-    with open('corpora/test.txt', 'r') as f:
-        words = f.read().replace('\n', ' ').split()
+    tokenized_text = []
+    pattern = re.compile(r'^[a-zA-Z]')
+    markov = {}
+    # Open and clean all texts in corpora folder
+    for name in os.listdir('corpora'):
+        if 'DS_Store' not in name:
+            with open(f'corpora/{name}', 'r') as f:
+                # Get rid of special characters
+                words = f.read().replace('\n', ' ').split()
+                for w in words:
+                    w = w.replace('"', '')
+                    w = w.replace('\'', '')
+                    if re.search(pattern, w):
+                        tokenized_text.append(w)
+    
 
-        # Get rid of special characters
-        pattern = re.compile(r'^[a-zA-Z]')
-        for w in words:
-            w = w.replace('"', '')
-            w = w.replace('\'', '')
-            if re.search(pattern, w):
-                cleaned_text.append(w)
-        
-    markov = create_markov(cleaned_text)
-    final_text = create_text(markov, 1000)
-    print(final_text)
+    # Create Markov Chain object if one doesn't exist
+    if os.path.exists('markov_chain.json'):
+        with open('markov_chain.json', 'r') as f:
+            print('Loading from JSON file: markov_chain.json')
+            markov = json.load(f)
+    else:
+        markov = create_markov(tokenized_text)
+        with open('markov_chain.json', 'w') as f:
+            json.dump(markov, f)
+    # Generate given number of verses
+    final_text = create_text(markov, 30)
+
+    with open('final.txt', 'w') as f:
+        f.write(final_text)
     return
 
 
-''' Given a text, create a Markov Chain object and return it '''
+''' Given a tokenized text, return a Markov Chain object '''
 def create_markov(text):
     markov_temp = {}
     markov_final = {}
+    total_length = len(text)
     i = 0
-    while i < len(text)-1:
+
+    # Go through the whole text
+    while i < total_length-2:
         current = text[i]
         next = text[i+1]
 
@@ -80,23 +77,24 @@ def create_markov(text):
             # Add one to total occurences
             markov_temp[current][1] += 1
             possible_words = markov_temp[current][0]
-
             # If the next word is already accounted for, add an occurence
             if next in list(possible_words.keys()):
                 possible_words[next] += 1
-            # Else create entry for the next word and set occurences to 1
+            # If the next word is not accounted for, create an entry
             else:
                 possible_words[next] = 1
-        # If current word not accounted for, add it
+        # If current word is not accounted for, create an entry
         else:
             markov_temp[current] = [
                 { next: 1 }, 
                 1
             ]
-
+        
         i += 1
+        # Loading screen
+        __stats(total_length, i)
     
-    # Create final entry for each word
+    # Create final object with all words and combos
     for w in list(markov_temp.keys()):
         possible_words = markov_temp[w][0]
         total_occurences = markov_temp[w][1]
@@ -114,14 +112,31 @@ def create_markov(text):
 
 ''' Given a Markov Chain object and length, return a generated text '''
 def create_text(markov, length):
-    text = ''
-    i = 0
+    verse = 1
+    end_verse = True
+    text = 'Book of '
+
+    # Create title
+    exclude = ['Israel', 'Egypt']
+    options = list(markov['of'].keys())
+    print(options)
+    option_weights = list(markov['of'].values())
+    while True:
+        choice = random.choices(
+                    options, 
+                    weights=option_weights, 
+                    k=1)[0]
+        # Must be a proper noun (exclude israel and egypt, too common)
+        if re.match(r'^[A-Z].*', choice) and choice not in exclude:
+            choice = ''.join(filter(str.isalpha, choice))   # Black magic
+            text += f'{choice}\n\n'
+            break
+
     # Choose a random word to start
     current_word = random.choice(list(markov.keys()))
 
     # Generate text
-    while i < length-1:
-        text += f'{current_word} '
+    while verse <= length:
         # List all possible next words
         possible_nexts = list(markov[current_word].keys())
         # List all weights associated with possible next words
@@ -131,10 +146,32 @@ def create_text(markov, length):
             possible_nexts, 
             weights=possible_nexts_weights, 
             k=1)[0]
+        
+        chance = random.randrange(0, 3)
+        # If verse has ended, start a new one
+        if end_verse:
+            text += f'[{verse}] {current_word.title()} '
+            end_verse = False
+        # If a word ends with punctuation, possibly end this verse
+        elif re.match(r'.*[^a-zA-Z]$', current_word) and chance == 0:
+            text += f'{current_word}\n'
+            end_verse = True
+            verse += 1
+        # Else, add a new word to the verse
+        else:
+            text += f'{current_word} '
+
         current_word = next_word
-        i += 1
 
     return text
+
+
+def __stats(length, i):
+    ''' Display loading messages '''
+
+    print(f'\tCreating Markov Chains                   \n'
+          f'\t{round((i / length) * 100, 3)}% done     \n',
+          end='\r\033[A\r\033[A')
 
 
 if __name__ == '__main__':
